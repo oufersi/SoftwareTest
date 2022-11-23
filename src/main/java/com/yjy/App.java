25211eb59ea68d20586db23a42e26bade62b063d
yjy <2455416393@qq.com>
2022-11-23 20:27:25 +0800

    first commit

--- src/main/java/com/yjy/App.java
+++ src/main/java/com/yjy/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+    public static int div(int a, int b){
+        return a / b;
     }
 }
--- src/test/java/com/yjy/AppTest.java
+++ src/test/java/com/yjy/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import static com.yjy.App.div;;
+
 /**
  * Unit test for simple App.
  */
@@ -13,8 +15,16 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void divTest(){
+        int returnVal;
+
+        try{
+            div(10, 0);
+
+            returnVal = div(10, 2);
+            assertTrue("result error", returnVal == 5);
+        } catch(Exception e){
+            assertTrue(e.getMessage(), false);
+        }
     }
 }
